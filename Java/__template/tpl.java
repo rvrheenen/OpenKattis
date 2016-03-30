8b427d96041de42199c1805cf3ee7192bab85923
rvrheenen <rick.van.rheenen@gmail.com>
2016-03-30 12:03:06 +0200

    ...

--- Java/__template/tpl.java
+++ Java/__template/tpl.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-
 public class __NAME__ {
 
     public static void main(String[] args) throws IOException {
