966763e38eb3f34a82061407f658b7ce6fe34283
rvrheenen <rick.van.rheenen@gmail.com>
2016-04-15 18:25:53 +0200

    change template

---
+++ Java/.template/tpl.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class __NAME__ {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
