ea6dcf103b357c31a51f1900d95055b2606e8aec
rvrheenen <rick.van.rheenen@gmail.com>
2016-04-17 18:13:57 +0200

    Add IOchecker scripts

---
+++ scripts/iocheck_java.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#detect names of file and input files
+filename=($(ls *.java))
+testdataname=($(ls *.in))
+
+javac $filename
+
+for f in "${testdataname[@]}"; do
+    if [ -n "$filename" ] && [ -n "$testdataname" ]
+      then
+      #strip off the extensions
+      basename=${filename%.java}
+      dataname=${f%.in}
+      
+      #run
+      if java $basename < $testdataname > $dataname.output
+        then
+
+        #diff the judge's output and the program output
+        diffResult="$(diff -b $dataname.out $dataname.output)"
+      
+        if [ -n "$diffResult" ]
+          then
+            echo $f
+            echo $diffResult
+            echo "Solution is not correct."
+          else
+            echo "Solution is correct."
+        fi
+      else
+        echo "Compilation or run time error"
+      fi
+    else
+      echo "Missing files"
+    fi
+done
+
+#clean up
+rm *.class
+rm *.output
---
+++ scripts/iocheck_python.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#detect names of file and input files
+filename=($(ls *.py))
+testdataname=($(ls *.in))
+
+#if these files are present...
+for f in "${testdataname[@]}"; do
+    if [ -n "$filename" ] && [ -n "$testdataname" ]
+      then
+      #strip off the extensions
+      basename=${filename%.py}
+      dataname=${f%.in}
+      
+      #run
+      if cat $f | python3 $filename  > $dataname.output
+        then
+
+        #diff the judge's output and the program output
+        diffResult="$(diff -b $dataname.out $dataname.output)"
+      
+        if [ -n "$diffResult" ]
+          then
+            echo $f
+            echo $diffResult
+            echo "Solution is not correct."
+          else
+            echo "Solution is correct."
+        fi
+      else
+        echo "Compilation or run time error"
+      fi
+    else
+      echo "Missing files"
+    fi
+done
+
+#clean up
+rm *.output
