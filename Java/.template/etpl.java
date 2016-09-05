692b4a1445b772f87e06e1fcbddee7201f2b8564
rvrheenen <rick.van.rheenen@gmail.com>
2016-09-05 16:38:34 +0200

    add new java template

---
+++ Java/.template/etpl.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Template {
+
+	public static void main(String[] args) throws IOException {
+		IO io = new IO(System.in);
+
+		
+		
+		io.close();
+	}
+
+	static class IO extends PrintWriter {
+		static BufferedReader r;
+		static StringTokenizer t;
+
+		public IO(InputStream i) {
+			super(new BufferedOutputStream(System.out));
+			r = new BufferedReader(new InputStreamReader(i));
+			t = new StringTokenizer("");
+		}
+
+		public String next() throws IOException {
+			while (!t.hasMoreTokens()) {
+				t = new StringTokenizer(r.readLine());
+			}
+			return t.nextToken();
+		}
+
+		public int nextInt() throws IOException{
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		}
+
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		}
+	}
+}
