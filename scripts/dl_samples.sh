527e22d86557cc1c44e778843140306f3e8d13dc
rvrheenen <rick.van.rheenen@gmail.com>
2016-09-05 16:36:57 +0200

    fix dl_samples script to handle lowercase

--- scripts/dl_samples.sh
+++ scripts/dl_samples.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-problem="${PWD##*/}"
+problem=`echo "${PWD##*/}" | tr '[:upper:]' '[:lower:]'`
 url="https://open.kattis.com/problems/$problem/file/statement/samples.zip"
 curl $url > samples.zip
 unzip samples.zip
