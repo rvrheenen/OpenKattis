8d6af1e151b85670216797cdbb44113ac4a5eb5e
rvrheenen <rick.van.rheenen@gmail.com>
2018-05-30 14:28:18 +0200

    add set_aliases script

---
+++ scripts/set_aliases.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# when sourcing or running in the directory, make sure to add the .
+# ie: source ./set_aliases.sh
+
+SCRIPT_DIR=$( cd ${0%/*} && pwd -P )
+
+alias dl_samples="${SCRIPT_DIR}/dl_samples.sh"
+alias iocheck_python="${SCRIPT_DIR}/iocheck_python.sh"
+alias iocheck_java="${SCRIPT_DIR}/iocheck_java.sh"
+alias iocheck_c="${SCRIPT_DIR}/iocheck_c.sh"
+alias iocheck_c++="${SCRIPT_DIR}/iocheck_c++.sh"
